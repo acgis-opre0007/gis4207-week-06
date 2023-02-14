9b5e7272ee4f825c0882b4366b7c6892ccb44db6
Laura Oprescu <opre0007@algonquinlive.com>
2023-02-14 17:06:01 -0500

    updated 1

--- LauraO_MariaT/batch_clipper.py
+++ LauraO_MariaT/batch_clipper.py
@@ -1,38 +1,58 @@
 import sys
-import arcpy
 import os
 
-arcpy.env.workspace = r'..\..\..\..\data\BatchClipData'
-
-sites = r'..\..\..\..\data\BatchClipData\Sites'
-clip = r'..\..\..\..\data\BatchClipData\TargetData'
-out = r'..\..\..\..\data\BatchClipData\output'
-
-feature_class = arcpy.ListFeatureClasses()
-
-for fc in feature_class:
-    print(fc)
+#arcpy.env.workspace = r'..\..\..\..\data\BatchClipData'
 
 def main():
+
+    global arcpy, env
 
     if len(sys.argv) !=4:
         print("Usage: batch_clipper.py <InWorkspace> <ClipWorkspace> <OutputWorkspace>")
         sys.exit()
     
-    if not arcpy.Exists(arcpy.env.workspace):
-        print("The feature class does not exist")
-        sys.exit()
+    #if not arcpy.Exists(file):
+        #print("The feature class does not exist")
+        #sys.exit()
 
-    InWorkspace = sys.argv[1]
-    ClipWorkspace = sys.argv[2]
-    OutWorkspace = sys.argv[3] 
+    import arcpy
+    from arcpy import env
+
+    In_Workspace = sys.argv[1]
+    Clip_Workspace = sys.argv[2]
+    Out_Workspace = sys.argv[3] 
+
+    env.workspace = In_Workspace
+    fc_list = []
+    in_F_class = arcpy.ListFeatureClasses()
+    for fc in in_F_class:
+        fc_list.append(fc)
+        print(os.path.join(os.path.abspath(In_Workspace), fc))
+        
+    env.workspace = Clip_Workspace
+    clip_F_class = arcpy.ListFeatureClasses()
+    for fc_clip in clip_F_class:
+        #nested for loop
+        print(fc_clip)
+        print(os.path.join(os.path.abspath(Clip_Workspace), fc_clip))
+
+    env.workspace = Out_Workspace
+    out_f_class = arcpy.ListFeatureClasses()
+    for fc_out in out_f_class:
+        print(fc_out)
+        print(os.path.join(os.path.abspath(Out_Workspace), fc_out))
+    
+    clip_analysis(in_features, clip_features, out_features)
+
 
 def clip_analysis(in_features, clip_features, out_features):
-    in_features = "CaribouLower.shp"
-    clip_features = "A1.shp", "A2.shp", "A3.shp"
-    out_features = out
+    in_features = r'..\..\..\..\data\BatchClipData\TargetData\CaribouLower.shp'
+    clip_features = r'..\..\..\..\data\BatchClipData\Sites'
+    out_features = r'..\..\..\..\data\BatchClipData\output'
 
     arcpy.analysis.Clip(in_features, clip_features, out_features)
+    for fc in clip_features:
+        print(os.path.join(os.path.abspath(in_features, fc)))
     
 
 if __name__ == "__main__":
