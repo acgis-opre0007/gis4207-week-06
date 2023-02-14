c51003dcf50e6baf05967c090672331730cd3b33
Laura Oprescu <opre0007@algonquinlive.com>
2023-02-14 15:49:40 -0500

    Temporary changes

--- LauraO_MariaT/batch_clipper.py
+++ LauraO_MariaT/batch_clipper.py
@@ -9,16 +9,8 @@ clip = r'..\..\..\data\BatchClipData\TargetData'
 out = r'..\..\..\data\BatchClipData\output'
 
 feature_class = arcpy.ListFeatureClasses()
-for fc in feature_class:
-    fc_des = arcpy.Describe(feature_class)
-    print(fc_des)
-
-
 
 for fc in feature_class:
-    arcpy.CopyFeatures_management(
-        fc, os.path.join(out, os.path.splitext(fc)[0]))
-    os.path.join()
     print(fc)
 
 def main():
@@ -40,7 +32,7 @@ def clip_analysis(in_features, clip_features, out_features):
     clip_features = "A1.shp", "A2.shp", "A3.shp"
     out_features = out
 
-    arcpy.analysis.Clip(in_features,  clip_features, out_features)
+    arcpy.analysis.Clip(in_features, clip_features, out_features)
     
 
 if __name__ == "__main__":
