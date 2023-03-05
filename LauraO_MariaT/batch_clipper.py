76078b9263a969810f334f3ea2457701a78e690a
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-03-05 15:28:55 -0500

    batch_clip working progress

--- LauraO_MariaT/batch_clipper.py
+++ LauraO_MariaT/batch_clipper.py
@@ -1,59 +1,28 @@
 import sys
-import os
+import os.path
+import arcpy
 
-#arcpy.env.workspace = r'..\..\..\..\data\BatchClipData'
 
 def main():
-
-    global arcpy, env
 
     if len(sys.argv) !=4:
         print("Usage: batch_clipper.py <InWorkspace> <ClipWorkspace> <OutputWorkspace>")
         sys.exit()
     
-    #if not arcpy.Exists(file):
-        #print("The feature class does not exist")
-        #sys.exit()
-
-    import arcpy
-    from arcpy import env
-
+    arcpy.env.overwriteOutput = True
+    
     In_Workspace = sys.argv[1]
     Clip_Workspace = sys.argv[2]
     Out_Workspace = sys.argv[3] 
 
-    env.workspace = In_Workspace
-    fc_list = []
-    in_F_class = arcpy.ListFeatureClasses()
-    for fc in in_F_class:
-        fc_list.append(fc)
-        print(os.path.join(os.path.abspath(In_Workspace), fc))
+    arcpy.env.workspace = In_Workspace
+    In_Featclass = arcpy.ListFeatureClass()
         
-    env.workspace = Clip_Workspace
-    clip_F_class = arcpy.ListFeatureClasses()
-    for fc_clip in clip_F_class:
-        #nested for loop
-        print(fc_clip)
-        print(os.path.join(os.path.abspath(Clip_Workspace), fc_clip))
-
-    env.workspace = Out_Workspace
-    out_f_class = arcpy.ListFeatureClasses()
-    for fc_out in out_f_class:
-        print(fc_out)
-        print(os.path.join(os.path.abspath(Out_Workspace), fc_out))
-    
-    clip_analysis(in_features, clip_features, out_features)
-
-
-def clip_analysis(in_features, clip_features, out_features):
-    in_features = r'..\..\..\..\data\BatchClipData\TargetData\CaribouLower.shp'
-    clip_features = r'..\..\..\..\data\BatchClipData\Sites'
-    out_features = r'..\..\..\..\data\BatchClipData\output'
-
-    arcpy.analysis.Clip(in_features, clip_features, out_features)
-    for fc in clip_features:
-        print(os.path.join(os.path.abspath(in_features, fc)))
-    
-
-if __name__ == "__main__":
-    main()
+    arcpy.env.workspace = Clip_Workspace
+    for Clip_FC in arcpy.ListFeatureClass():
+        for FClass in In_Featclass:
+            new_name = Clip_FC.split('.') + "_" + FClass.split('.')[0]
+            arcpy.analyze.Clip(os.path.join(In_Workspace, FClass), os.path.join(Clip_Workspace, Clip_FC), Out_Workspace + '\\' + new_name)
+                  
+    if __name__ == "__main__":
+         main()
