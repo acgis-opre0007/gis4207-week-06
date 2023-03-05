5dcf24258af89c70743f17702d15f9e92c72ccc0
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-03-05 14:51:43 -0500

    Starter

---
+++ LauraO_MariaT/data_prep.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def main():
+
+    if len(sys.argv) !=4:
+        print("Usage: data_prep.py <in_gdbs_base_folder> <out_gdb> <out_feature_dataset")
+        sys.exit()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    if __name__ == "__main__":
+            main()
