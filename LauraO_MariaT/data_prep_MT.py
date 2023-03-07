62b76cdfabe9ba465600bb3bfd8d4e7d44f845a0
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-03-06 23:04:49 -0500

    small fix made

--- LauraO_MariaT/data_prep_MT.py
+++ LauraO_MariaT/data_prep_MT.py
@@ -22,8 +22,8 @@ def main():
     arcpy.env.workspace = in_gdbs_folder
     
     workspace_path = arcpy.ListWorkspaces()
+    arcpy.env.workspace = ws
     for ws in workspace_path[:1]:
-        arcpy.env.workspace = ws
         FeatureClass_List = arcpy.ListFeatureClasses()
         desc = arcpy.Describe(FeatureClass_List) 
              
@@ -31,14 +31,14 @@ def main():
         
     FeatureClass_path = arcpy.CreateFeatureDataset_management(Geodatabase_path, out_feature_dataset,sr)[0]
     
+    arcpy.env.workspace = ws
     for ws in workspace_path[:1]:
-       arcpy.env.workspace = ws
        featureClasses = arcpy.ListFeatureClasses
        desc = arcpy.Describe(featureClasses[0])
        arcpy.FeatureClassToFeatureClass_conversion(in_feature = featureClasses[0], out_feature = FeatureClass_path, out_name = f"{desc.baseName}")
     
     print(Geodatabase_path,"was created")   
-    print(FeatureClass_path, "was created")
+    print(FeatureClass_path, "was created") 
                
 
 if __name__ == '__main__':
